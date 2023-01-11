b0d9ad0d25392cc93535fad9c5a4a8340b9af8ec
ThierryC <thierrycohnen@gmail.com>
2023-01-11 14:30:33 +0100

    12

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React yesssssssssssssssssssssss and tooooooooooooooo yyyyyyyyyyyyyy
+          Learn React really
           oh oui
         </a>
       </header>
